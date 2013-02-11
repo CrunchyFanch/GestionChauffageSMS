e3d389c3ca1ffdd78c8c47f7425fe15ee7f62bc4
François Canet <francois.canet@gmail.com>
2013-02-11 23:21:16 +0100

    utilsation library sim900.h et gsm.h de openelectronic refonte totale
    Signed-off-by: François Canet <francois.canet@gmail.com>

--- GestionChauffageSMSDidier.ino
+++ GestionChauffageSMSDidier.ino
@@ -1,78 +1,98 @@
+#include "SIM900.h"
 #include <SoftwareSerial.h>
-#include <String.h>
+#include "sms.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
- 
-#define NUM_GSM_DEST "+33650829390" //numero pour envoyer les sms d'acquittement
-#define TIMEOUT_AT 20000 //timeout de temps de reponse d'une commande AT pour eviter boucle infini
-#define DELAI_SCRUTATION_SMS 3000//intervalle de temps ou sont lu les sms non lu
 
 #define ONE_WIRE_BUS 6
-
-OneWire oneWire(ONE_WIRE_BUS);
-
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
 
 #define PIN_C1 2 //defini les sortie qui doivent allimenter les telerupteurs !! ne pas utiliser 0,1,7,8 (port serie)
 #define PIN_C2 3
 #define PIN_C3 4
 #define PIN_C4 5
 
-#define PIN_RETOUR_C1 10 //defini les entrÃ©es sur laquelle on lit l'etat du chauffage
-#define PIN_RETOUR_C2 11
-#define PIN_RETOUR_C3 12
-#define PIN_RETOUR_C4 13
+#define PIN_RETOUR_C1 A0 //defini les entres sur laquelle on lit l'etat du chauffage
+#define PIN_RETOUR_C2 A1
+#define PIN_RETOUR_C3 A2
+#define PIN_RETOUR_C4 A3
 
 #define DUREE_IMPULS 3000
 
-
-#define COMMANDE_C1 "TELERUPT_C1" //Definition des mot cle a envoyer par sms pour effectuer une action sur les chauffage
-#define COMMANDE_C2 "TELERUPT_C2"
-#define COMMANDE_C3 "TELERUPT_C3"
-#define COMMANDE_C4 "TELERUPT_C4"
-
-#define SMS_CMD_C1 "IMPULSION SUR C1"
-#define SMS_CMD_C2 "IMPULSION SUR C2"
-#define SMS_CMD_C3 "IMPULSION SUR C3"
-#define SMS_CMD_C4 "IMPULSION SUR C4"
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
 
-#define COMMANDE_ETAT_CHAUFFAGE "ETAT_CHAUFF" //mot cle pour lister l'etat des chauffage
+char temp_str[5]; //stocke la temperature
+
+boolean started=false;
 
 
 
-
-SoftwareSerial GSM(7, 8);
-
-boolean SmSaEnvoyer = false;
-
-
-void setup()
+void setup() 
 {
   pinMode(PIN_C1,OUTPUT); //mode des pin en sortie
   pinMode(PIN_C2,OUTPUT);
   pinMode(PIN_C3,OUTPUT);
   pinMode(PIN_C4,OUTPUT);
   
-  pinMode(PIN_RETOUR_C1,INPUT_PULLUP); //mode des pin en entree
+ /* pinMode(PIN_RETOUR_C1,INPUT_PULLUP); //mode des pin en entree avec activation des resistances de PULLUP interne à l'arduino
   pinMode(PIN_RETOUR_C2,INPUT_PULLUP);
   pinMode(PIN_RETOUR_C3,INPUT_PULLUP);
-  pinMode(PIN_RETOUR_C4,INPUT_PULLUP);
+  pinMode(PIN_RETOUR_C4,INPUT_PULLUP);*/
   
-  GSM.begin(9600);
+  
+  LireTemp(temp_str);   //lance la lecture de temperature
+  
   Serial.begin(9600);
-  delay(2000);
-  Serial.println("REBOOT!! ");
-  
 
-  GSM.println("AT+IPR=9600");
-  delay(200);
-  GSM.println("AT+CMGD=0,4"); // commande At du type +CMGD=index[,flag] pour effacer tout les sms ( du type "received read", "stored unsent" or "stored sent" )lu index est ignorÃ©
-  delay(200);
-  //GSM.flush();
+
+  if (gsm.begin(2400)){
+    Serial.println("\nModule pret");
+    started=true;  
+  }
+  else Serial.println("\nEchec");
+  
+  if(started){
+
+  }
+
+};
+
+void loop() 
+{
+
+SMSGSM ModuleSms;
+char phone_num[20]; // tableau numero
+char sms_text[160]; // tableau globale pour le texte des  SMS recus
+char sms_envoi[160];// tableau globale pour le texte des  SMS a envoyer
+
+  LireTemp(temp_str);
+  delay(2000);
+  
+  char pos = ModuleSms.IsSMSPresent(SMS_UNREAD);
+  if (pos) {
+
+    ModuleSms.GetSMS(pos, phone_num, sms_text, 160);
+    Serial.println(phone_num);
+    Serial.println(sms_text);
+    Serial.println(pos,DEC);
+    Chauffage(sms_text,sms_envoi);
+    strcat(sms_envoi," Temp="); //rajout de la temperature
+    strcat(sms_envoi,temp_str);
+    Serial.println("\nSMS a envoyer:");
+    Serial.println(sms_envoi);
+    if (ModuleSms.SendSMS(phone_num,sms_envoi)){
+         Serial.println("\nSMS envoye:");
+         Serial.println(sms_envoi);
+    }
+   
+  }else{
+
+    Serial.println("pas de sms");
+    }    
+   
 }
- 
+  
 
 
 void TelerupteurImpuls(int pin,int duree){
@@ -82,160 +102,77 @@ void TelerupteurImpuls(int pin,int duree){
 
 }
   
-float LireTemp(){
-    float t ;
-    sensors.requestTemperatures();
-           
-     t = sensors.getTempCByIndex(0);
-    return t;
+void LireTemp(char tabt[]){ // fonction qui lit la temperature sur la sonde Dallas DS1820 (protocole onewire)
+    
+    sensors.requestTemperatures();           
+     delay(5);
+   float  t = sensors.getTempCByIndex(0);
+    dtostrf(t,5,2,tabt);
+    Serial.println("Temp=");
+    Serial.println(tabt);
+    
     
   }
 
-void Chauffage(char tab[]){
+void Chauffage(char tab[],char sms[]){ //fonction qui lit le sms en entree et effectue une action sur un des 4 chauffage ou retourne l'etat si un mot clé est détécté 
     
-    String Temperature ="";
-    String retour = "";
+
+    char retour[80];
+   
     boolean commandeok = false;
     float tc;
-  
-       if (Contient(tab,COMMANDE_C1)){
+       if (strcmp(tab,"TELERUPT_C1") == 0){
            TelerupteurImpuls(PIN_C1,DUREE_IMPULS);
-           retour = SMS_CMD_C1;
+           strcpy(retour,"Impulsion envoyee sur C1!");
            commandeok = true; 
           
        }
-       if (Contient(tab,COMMANDE_C2)){
+       if (strcmp(tab,"TELERUPT_C2") == 0){
            TelerupteurImpuls(PIN_C2,DUREE_IMPULS);
-           retour = SMS_CMD_C2;
+           strcpy(retour,"Impulsion envoyee sur C2!");
            commandeok = true; 
           
        }
-       if (Contient(tab,COMMANDE_C3)){
+       if (strcmp(tab,"TELERUPT_C3") == 0){
            TelerupteurImpuls(PIN_C3,DUREE_IMPULS);
-           retour = SMS_CMD_C3;
+           strcpy(retour,"Impulsion envoyee sur C3!");
            commandeok = true; 
            
        }
-       if (Contient(tab,COMMANDE_C4)){
+       if (strcmp(tab,"TELERUPT_C4") == 0){
            TelerupteurImpuls(PIN_C4,DUREE_IMPULS);
-           retour = SMS_CMD_C4;
+          strcpy(retour,"Impulsion envoyee sur C4!");
            commandeok = true; 
           
        }
-       if (Contient(tab,COMMANDE_ETAT_CHAUFFAGE)){
+       if (strcmp(tab,"INFO") == 0){
           
-           retour = String("Etat chauffage: ");
-           if (digitalRead(PIN_RETOUR_C1)) retour += String("OFF,");
-           else retour += String("ON,");
-           if (digitalRead(PIN_RETOUR_C2)) retour += String("OFF,");
-           else retour += String("ON,");
-           if (digitalRead(PIN_RETOUR_C3)) retour += String("OFF,");
-           else retour += String("ON,"); 
-           if (digitalRead(PIN_RETOUR_C4)) retour += String("OFF");
-           else retour += String("ON");                       
-           retour += String(", T= ");
-           tc =  LireTemp();
-         //  retour += String(tc);        
+         strcpy(retour,"Chauffage: ");
+        if (analogRead(PIN_RETOUR_C1) < 500) strcat(retour,"C1=OFF,");
+           else strcat(retour,"C1=ON,");
+           delay(5);
+           if (analogRead(PIN_RETOUR_C2) < 500) strcat(retour," C2=OFF,");
+           else strcat(retour," C2=ON,");
+           delay(5);
+           if (analogRead(PIN_RETOUR_C3) < 500) strcat(retour," C3=OFF,");
+           else strcat(retour," C3=ON,");
+           delay(5);
+            if (analogRead(PIN_RETOUR_C4) < 500) strcat(retour," C2=OFF");
+           else strcat(retour," C3=ON");
+           delay(5);
+       
            commandeok = true; 
 
        }
    
       if (commandeok == false){
     
-        retour = "Commande non reconnue!";
+         strcpy(retour,"Commande non reconnue!");
         
       }
 
-      retour.toCharArray(tab,512);
-
-}
-
-void LireSerialData(char tab[])
-{
-    char c;
-    int i = 0;
-
-   if (GSM.available()){
-     
-    while(GSM.available() > 0){
-      c = (char)GSM.read();
-          if (c>=32 && c<= 126)
-      {
-        //Serial.write(c);
-        //delay(1);
-        tab[i] = c;
-       i++; 
-      }
-    }
-  tab[i] = '\0'; 
- }
-
-
-}
-void loop(){
- 
-  int ind1,ind2;
-   char buf[512] = "";
- //String S0,S1,S2;
-  delay(2000);
- 
-   
-   LireSerialData(buf);
-   Serial.print(buf);
-   if (Contient(buf,"+CMT:")){ //un sms a ete recu
-       Serial.println("SMS recu!!!:");
-       FiltreSMS(buf);
-       Serial.println(buf);
-       Chauffage(buf);       
-        delay(200);
-        Serial.println("SMS reponse:");
-        GSM.print("AT+CMGF=1\r");
-        delay(200);     
-        GSM.print("AT + CMGS = \"");
-        delay(200);
-        GSM.print(NUM_GSM_DEST); //numero mobile destinataire
-        delay(200);
-        GSM.println("\"");
-        delay(200);
-        GSM.print(buf);//contenu du message
-        delay(200);
-        GSM.println((char)26);// ASCII de ctrl-Z (26)
-        delay(200);
-        GSM.println();
-        buf[0] = '\0';
-        LireSerialData(buf);
-
-   }
-   
- 
+  strcpy(sms,retour);
 
 }
 
 
-boolean Contient(char tab[], String trouve) {
-    String s = String(tab);
-    boolean vrai = 0;
-    int maxi = s.length() - trouve.length();
-    Serial.println("s="+s);
-    Serial.println("trouve="+trouve);
-    if (s.indexOf(trouve)>=0) vrai = 1;
-    else vrai= 0;    
-     return vrai;
-}
-
-void FiltreSMS(char tab[]){
-  String s = String(tab);
-  String t;
-  int i;
-    i = s.lastIndexOf('\"');
-    t = s.substring(i+1); 
-    t.toCharArray(tab,512);
-  }
-
-
-void EnvoyerSms(char message[],char numero[])
-{
-
-
-
-}
